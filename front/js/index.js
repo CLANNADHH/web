4b64e2e0e8eaa635a7e3cc6bd4c486b44951d6af
HH <clannadhh@gmail.com>
2018-09-06 00:14:39 +0800

    更新了注册跟登录页面

---
+++ front/js/index.js
@@ -0,0 +1,10 @@
+
+
+$(function () {
+    $(".register").click(function () {
+        location.href = "http://127.0.0.1:8080/register.html"
+    })
+    $(".login").click(function () {
+        location.href = "http://127.0.0.1:8080/login.html"
+    })
+})
--- hhblog/hhblog/settings.py
+++ hhblog/hhblog/settings.py
@@ -134,3 +134,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.163.com'
+EMAIL_PORT = 25
+#发送邮件的邮箱
+EMAIL_HOST_USER = 'clannadhh@163.com'
+#在邮箱中设置的客户端授权密码
+EMAIL_HOST_PASSWORD = 'clannad163'
+#收件人看到的发件人
+EMAIL_FROM = '个人<clannadhh@163.com>'
